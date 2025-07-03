f43fc32d4059f107f4f96892df2db77e4b305d86
yenulinehansa <yenulinehansa739@gmail.com>
2025-07-03 12:51:07 +0530

    Final Submission

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-fetch("https://jsonplaceholder.typicode.com/todos/2").then(res => res.json()).then(data => { console.log(data); })
+
+
+
+/*function SearchName() {
+    let countryName = document.getElementById("name").value;
+    fetch("https://restcountries.com/v3.1/name/" + countryName)
+        .then(res => res.json())
+        .then(data => {
+            let root = document.getElementById("root");
+            data.forEach(element => {
+                root.innerHTML = `<section>
+                                    <img src="${element.flags.png}" alt="">
+                                    <h3>${element.name.official}</h3>
+                                    <p>${element.capital[0]}</p>
+                                    <p>${element.region}</p>
+
+                            </section>
+
+            `
+            });
+
+
+        })
+
+}*/
+
+localStorage.setItem("name", "saman");
+
+let customerName = localStorage.getItem("name");
+
+console.log(customerName);
